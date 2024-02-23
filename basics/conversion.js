9eeab2e51818d38d8da8a1d7f513cb8dee6a03d4
codebyshoaib <shobbi800@gmail.com>
2024-02-24 00:32:14 +0500

    conversions-2

--- basics/conversion.js
+++ basics/conversion.js
@@ -1,14 +1,35 @@
-let score=33;
-console.log(typeof(score));
-let ValNumber=Number(score); //type converted to number
-console.log(typeof(ValNumber));
-console.log(ValNumber);
+// let score=33;
+// console.log(typeof(score));
+// let ValNumber=Number(score); //type converted to number
+// console.log(typeof(ValNumber));
+// console.log(ValNumber);
 
-//"33" => 33
-//"3abc" => NaN
-//null => 0
+// //"33" => 33
+// //"3abc" => NaN
+// //null => 0
 
-let someNum=33;
-let str=String(someNum);
-console.log(str);
-console.log(typeof(str));
+// let someNum=33;
+// let str=String(someNum);
+// console.log(str);
+// console.log(typeof(str));
+
+
+//      operations  //
+let val=3;
+let negVal=-val;
+//console.log(negVal);
+//basic maths +,-,*,/,%
+let name=12;
+let name2="2";
+console.log(name+name2);
+
+//preefix operator
+//postfix operator
+
+//comparisons 
+//<,>,>=,<=
+//different datatype? unpredictable 
+//typescript => strict Javascript
+
+// ===  => checks datatypes too
+//console.log(undefined == '12');
