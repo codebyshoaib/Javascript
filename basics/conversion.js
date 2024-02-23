8f2f9a7e8111236a13377e077a6608439cf5879c
codebyshoaib <shobbi800@gmail.com>
2024-02-24 00:16:45 +0500

    type conversions

---
+++ basics/conversion.js
@@ -0,0 +1,14 @@
+let score=33;
+console.log(typeof(score));
+let ValNumber=Number(score); //type converted to number
+console.log(typeof(ValNumber));
+console.log(ValNumber);
+
+//"33" => 33
+//"3abc" => NaN
+//null => 0
+
+let someNum=33;
+let str=String(someNum);
+console.log(str);
+console.log(typeof(str));
