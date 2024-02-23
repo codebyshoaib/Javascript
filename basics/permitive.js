e8b49b1edf0267143190ebe7b5c1ffd4d5447ec8
codebyshoaib <shobbi800@gmail.com>
2024-02-24 00:40:40 +0500

    permitive and non-permitive datatypes

---
+++ basics/permitive.js
@@ -0,0 +1,10 @@
+/* how values are stored in memory 
+
+1. permitive: copies are modified (call by value)
+    strings,numbers,boolean,null,symbol
+
+2. Non-permitive: modifies original value (call by reference)
+        arrays,objects,functions
+
+*/
+//JS is dynamically typed language ; type checking takes place at runtime or execution time
