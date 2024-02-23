1becb0e338d214d25838160b4a832ab814149403
codebyshoaib <shobbi800@gmail.com>
2024-02-24 00:50:12 +0500

    memory in javascript

---
+++ basics/memory.js
@@ -0,0 +1,22 @@
+/**
+ two types 
+ 1.Stack for permitives 
+ 2. Heap for non-permitive datatypes
+ **/
+
+//  let myname="shoaib";
+//  let name2=myname;
+//  name2='hello';
+//  console.log(myname);
+//  console.log(name2);
+
+
+// let userOne = {
+//     name:"Shoaib",
+//     email:"abc@gmail.com",
+//     number:12345
+// }
+// let userTwo = userOne;
+// userTwo.email="hehe"; //change in both
+// console.log(userOne);
+// console.log(userTwo);
