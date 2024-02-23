771af17827fc08e7bb8abfdbb46b38c2401bbd4a
codebyshoaib <shobbi800@gmail.com>
2024-02-24 00:05:00 +0500

    Data Types

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+ "use strict"; //treate the code as newer version of javascript
+ //alert(3+3); //runs on browser, otherwise error
+
+ // ECMA Script ==> standard organization
+ // MDN Documentation ==> By Mozilla
+/* Data Types 
+ Number
+ Bigint
+ string
+ booleans
+ null   ==> a value also ,representation of empty , object
+ undefined ==> value is not defined
+ symbol =>uniqueness of components
+
+
+ objects
+*/
+console.log(typeof(null)); //object
