ed0d87154b61bacd861933fd88a6a6f2122744aa
codebyshoaib <shobbi800@gmail.com>
2024-02-23 23:50:35 +0500

    message

---
+++ basics/letvar.js
@@ -0,0 +1,11 @@
+let username = 'shoaib';
+const account_Id=1234;
+var email_Address='Shobbi@gmail.com';
+city="Lahore";
+let account_state;  //undefined
+
+console.table([username,account_Id,email_Address,city,account_state]); //tabular display
+
+
+//var => No Block scope {} - issue
+//let => Block scope {} - preffered
