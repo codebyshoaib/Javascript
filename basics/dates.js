b6f663c6bf0fe6b823b18945f3122bb1797c7aa3
codebyshoaib <shobbi800@gmail.com>
2024-02-29 00:49:29 +0500

    dates

---
+++ basics/dates.js
@@ -0,0 +1,12 @@
+//let myDate=new Date(2024,0,3);
+let myDate=new Date('2024-01-3');
+// console.log(myDate.toTimeString()); // many methods here
+// console.log(typeof(myDate)) ; //object
+console.log(myDate);  //own defined date
+
+//timestamps . Date.now()
+//getTime(). getMonth, getDay...
+// myDate.toLocaleString('default',{
+//     weekday:'long'
+// })
+
